060c5f36ff89d8b2f1a7d79fdea3bf35b3869000
bibixx <bartosz@legiec.io>
2020-05-07 23:17:11 +0200

    Fix paths

--- UTP4/src/zad1/Main.java
+++ UTP4/src/zad1/Main.java
@@ -10,7 +10,7 @@ package zad1;
 public class Main {
   public static void main(String[] args) {
     String dirName = System.getProperty("user.home")+"/UTP6dir";
-    String resultFileName = "UTP6res1.txt";
+    String resultFileName = "UTP6res.txt";
     Futil.processDir(dirName, resultFileName);
   }
 }
--- UTP4/src/zad2/Main.java
+++ UTP4/src/zad2/Main.java
@@ -10,7 +10,7 @@ package zad2;
 public class Main {
   public static void main(String[] args) {
     String dirName = System.getProperty("user.home")+"/UTP6dir";
-    String resultFileName = "UTP6res2.txt";
+    String resultFileName = "UTP6res.txt";
     Futil.processDir(dirName, resultFileName);
   }
 }
