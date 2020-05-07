959d6fcd91f80c55ab3370cc2ba83dce43eb08f1
bibixx <bartosz@legiec.io>
2020-05-08 00:14:43 +0200

    Add zad3

--- UTP4/src/zad2/Futil.java
+++ UTP4/src/zad2/Futil.java
@@ -10,31 +10,20 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class Futil {
-
 	public static void processDir(String dirName, String resultFileName) {
 		// TODO Auto-generated method stub
 		Predicate<Path> isFile = Files::isRegularFile;
 		try {
-			Stream<String> stream = Files.walk(Paths.get(dirName))
-				.filter(isFile)
-				.flatMap(p -> {
-					try {
-						return Files.lines(
-							p,
-							Charset.forName("cp1250")
-						);
-					} catch (IOException e) {
-						e.printStackTrace();
-						return null;
-					}
-				})
-				.filter(l -> l != null);
-			
-			Files.write(
-				Paths.get("./" + resultFileName),
-				(Iterable<String>)stream::iterator,
-				StandardCharsets.UTF_8
-			);
+			Stream<String> stream = Files.walk(Paths.get(dirName)).filter(isFile).flatMap(p -> {
+				try {
+					return Files.lines(p, Charset.forName("cp1250"));
+				} catch (IOException e) {
+					e.printStackTrace();
+					return null;
+				}
+			}).filter(l -> l != null);
+
+			Files.write(Paths.get("./" + resultFileName), (Iterable<String>) stream::iterator, StandardCharsets.UTF_8);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- UTP4/src/zad3/Main.java
+++ UTP4/src/zad3/Main.java
@@ -6,8 +6,48 @@
 
 package zad3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+	URL url = new URL("http://wiki.puzzlers.org/pub/wordlists/unixdict.txt");
+	BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+	LinkedHashMap<String, ArrayList<String>> map = new LinkedHashMap<String, ArrayList<String>>();
+	
+	reader
+		.lines()
+		.forEach(str -> {
+			char[] arr = str.toCharArray(); 
+		    Arrays.sort(arr);
+		    String sorted = String.valueOf(arr);
+
+			if (map.get(sorted) == null) {
+				map.put(sorted, new ArrayList<String>());
+			}
+
+			map.get(sorted).add(str);
+		});
+	
+	int maxSize = map
+		.entrySet()
+		.stream()
+		.max((e1, e2) -> Integer.compare(e1.getValue().size(), e2.getValue().size()))
+		.get()
+		.getValue()
+		.size();
+
+	map
+		.entrySet()
+		.stream()
+		.filter(e -> e.getValue().size() == maxSize)
+		.map(e -> e.getValue().stream().collect(Collectors.joining(", ")))
+		.forEach(e -> System.out.println(e));
   }
 }
