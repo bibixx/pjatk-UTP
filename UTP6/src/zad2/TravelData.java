e5d097ea163d505f9b385e46eab9c245f6e0c5a6
bibixx <bartosz@legiec.io>
2020-06-05 22:29:12 +0200

    Remove unsued import

--- UTP6/src/zad2/TravelData.java
+++ UTP6/src/zad2/TravelData.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.text.NumberFormat;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
